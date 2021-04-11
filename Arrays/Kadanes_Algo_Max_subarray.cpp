03cd7b843f5eecdfa720539fca30223543f4ab6e
Neha Kale <nkale8013@gmail.com>
2021-04-11 11:13:51 +0530

    kadanes algorithm for finding max contigious subarray

---
+++ Arrays/Kadanes_Algo_Max_subarray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int max_current,  max_global;
+    max_current=max_global = arr[0];
+
+    for(int i=1; i<n; i++){
+        max_current = max(arr[i], max_current + arr[i]);
+
+        if(max_current > max_global)
+            max_global = max_current;
+    }
+    cout<<max_global<<endl;
+    return 0;
+}
