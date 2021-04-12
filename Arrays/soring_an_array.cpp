d6bd43b9185d8fd337d469ce6d2dc1c73f808353
Neha Kale <nkale8013@gmail.com>
2021-04-12 11:04:31 +0530

    sorting array of 0, 1, 2 without using sorting algo

--- Arrays/soring_an_array.cpp
+++ Arrays/soring_an_array.cpp
@@ -1,11 +1,30 @@
-class Solution
-{
-    public:
-    void sort012(int a[], int n)
-    {
-        // coode here 
-        sort(a, a+n);
-        
-    }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
     
-};
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+
+    while(mid<=high){
+        if(arr[mid] == 0){
+            swap(arr[low++], arr[mid++]);
+        }else if(arr[mid] == 1){
+            mid++;
+        }else if(arr[mid] == 2){
+            swap(arr[mid], arr[high--]);       
+        }
+    }
+
+    for(int i=0; i<n; i++)
+        cout<<arr[i];
+
+        
+}
