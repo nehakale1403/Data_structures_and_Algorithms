ccb812a199dfb97fff6563d8ccb2fb137c32e52a
Neha Kale <nkale8013@gmail.com>
2021-04-19 13:02:09 +0530

    finding longest contigious sequence

--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -3,34 +3,13 @@ using namespace std;
 
 int main(){
 
-    int n;
-    cin>>n;
-
-    int arr[100000] = {0};
-    arr[0] = 1;
-    int q=2;
-    int len = 1;
-    int x = 0;
-    int num = 0;
-
-    while(q<=n){
-        
-        x=0;
-        num=0;
-        while(x<len){
-            arr[x] = arr[x] * q;
-            arr[x] = arr[x]+num;
-            num = arr[x]/10;
-            arr[x] = arr[x]%10;
-            x++;
-        }
-        while(num!=0){
-            arr[len] = num%10;
-            num = num/10;
-            len++;
-        }
-        q++;
-    }
-    for(int i=len-1; i>=0; i--)
-        cout<<arr[i];
+    int a = 5;
+    int *p = &a;
+    int **q = &p;
+    cout<<a<<endl; //5
+    cout<<p<<endl; //addr a
+    cout<<q<<endl; //addr p
+    cout<<*p<<endl;   //5
+    cout<<*q<<endl; //addr p
+    cout<<**q<<endl; //5
 }
