91212620131b367034a495ef81e729d213a5c56a
Neha Kale <nkale8013@gmail.com>
2021-05-23 11:06:18 +0530

    KMP algorithm for longest prefix and suffix

--- Strings/65_KMP_Longest_prefix_and_suffix.cpp
+++ Strings/65_KMP_Longest_prefix_and_suffix.cpp
@@ -1,3 +1,8 @@
+//Knuth Morris Pratt (KMP) is an algorithm, which checks the characters from left to right. 
+// When a pattern has a sub-pattern appears more than one in the sub-pattern, 
+// it uses that property to improve the time complexity, also for in the worst case.
+// The time complexity of KMP is O(n).
+
 #include<bits/stdc++.h>
 using namespace std;
 
