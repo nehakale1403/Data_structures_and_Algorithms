30e4a1e6df63ee7807a1683d648581852c346775
Neha Kale <nkale8013@gmail.com>
2021-06-14 12:52:52 +0530

    book allocation problem using binary search

--- Searching_and_Sorting/114_Book_allocation_problem.cpp
+++ Searching_and_Sorting/114_Book_allocation_problem.cpp
@@ -5,11 +5,14 @@ bool solve(int arr[], int n, int mid, int k){
     int sum = 0;
     int stu=1;
     for(int i=0; i<n; i++){
+        //if the book is having pages greater than mid
         if(arr[i]>mid)
             return false;
+        //if sum of books for single student is greater than mid, we will take the next student
         if(sum+arr[i]>mid){
             stu++;
             sum=arr[i];
+            //if students exceeds the required students
             if(stu>k)
                 return false;
         }else{
@@ -21,20 +24,26 @@ bool solve(int arr[], int n, int mid, int k){
 
 int findPages(int arr[], int n, int k){
 
+    sort(arr, arr+n);
+
     int lb = 0;
     int sum = 0;
     int ans=0;
 
+    //we will apply binary search on the sum
     for(int i=0; i<n; i++)
         sum+=arr[i];
     
     int ub = sum;
     while(lb<=ub){
         int mid = (lb+ub)/2;
+        //checking if we can allocate 'mid' number of pages to k students
         if(solve(arr, n, mid, k)){
+            //if yes, we will try to minimize the pages
             ans = mid;
             ub = mid-1;
         }else{
+            //if not we will check for more number of pages
             lb = mid+1;
         }
     }
