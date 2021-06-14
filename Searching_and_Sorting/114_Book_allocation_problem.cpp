7e51547a04e1d7331ea5c3f0f3affefbe4b69151
Neha Kale <nkale8013@gmail.com>
2021-06-14 21:44:26 +0530

    book allocation problem using binary search

--- Searching_and_Sorting/114_Book_allocation_problem.cpp
+++ Searching_and_Sorting/114_Book_allocation_problem.cpp
@@ -2,21 +2,30 @@
 using namespace std;
 
 bool solve(int arr[], int n, int mid, int k){
+
+    cout<<"Mid ="<<mid<<endl;
     int sum = 0;
     int stu=1;
     for(int i=0; i<n; i++){
         //if the book is having pages greater than mid
-        if(arr[i]>mid)
+        if(arr[i]>mid){
+            cout<<arr[i]<<" is greater than "<<mid<<endl;
             return false;
+        }   
         //if sum of books for single student is greater than mid, we will take the next student
-        if(sum+arr[i]>mid){
+        if((sum+arr[i])>mid){
+            cout<<sum+arr[i]<<" is greater than "<<mid<<endl;
             stu++;
+            
             sum=arr[i];
+            cout<<"adding student "<<stu<<endl;
             //if students exceeds the required students
             if(stu>k)
                 return false;
         }else{
+            
             sum+=arr[i];
+            cout<<"adding in sum "<<sum<<endl;
         }
     }
     return true;
@@ -24,19 +33,18 @@ bool solve(int arr[], int n, int mid, int k){
 
 int findPages(int arr[], int n, int k){
 
-    sort(arr, arr+n);
-
     int lb = 0;
-    int sum = 0;
+    int s = 0;
     int ans=0;
 
     //we will apply binary search on the sum
     for(int i=0; i<n; i++)
-        sum+=arr[i];
+        s+=arr[i];
     
-    int ub = sum;
+    int ub = s;
     while(lb<=ub){
         int mid = (lb+ub)/2;
+        
         //checking if we can allocate 'mid' number of pages to k students
         if(solve(arr, n, mid, k)){
             //if yes, we will try to minimize the pages
