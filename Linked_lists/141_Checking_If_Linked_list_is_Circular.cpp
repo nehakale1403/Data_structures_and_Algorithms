a29b1c165130fbde0c429f734c6bea7094ec7297
Neha Kale <nkale8013@gmail.com>
2021-06-23 14:52:22 +0530

    checking if the linked list is circular or not

---
+++ Linked_lists/141_Checking_If_Linked_list_is_Circular.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+
+    public:
+    int data;
+    node *next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+
+    node *temp = new node(val);
+
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+        
+
+    node* p = head;
+
+    while(p->next != NULL)
+        p = p->next;
+    
+    p->next = temp;
+}
+
+void display(node* &head){
+
+    node *p = head;
+    while(p != NULL){
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+    cout<<endl;
+}
+
+void reverse(node* &head){
+    
+    node *p, *q, *r;
+    p = head;
+    q = r = NULL;
+
+    while(p != NULL){
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+    }
+    head = q;
+}
+
+int main(){
+
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    display(head);
+    reverse(head);
+    display(head);
+    
+    return 0;
+}
