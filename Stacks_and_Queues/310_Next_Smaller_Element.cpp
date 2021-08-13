986f24200c79be32185864ae8e7176316065fa84
Neha Kale <nkale8013@gmail.com>
2021-08-13 12:53:40 +0530

    Next smaller element

---
+++ Stacks_and_Queues/310_Next_Smaller_Element.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nextSmallerEle(int arr[], int n){
+
+    stack<int> st;
+    st.push(arr[0]);
+
+    for(int i=1; i<n; i++){
+
+        if(st.empty()){
+            st.push(arr[i]);
+            continue;
+        }
+        while((!st.empty()) && st.top() > arr[i]){
+            cout<<arr[i]<<" ";
+            st.pop();
+        }
+        st.push(arr[i]);
+    }
+    while(!st.empty()){
+        cout<<"-1"<<" ";
+        st.pop();
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+
+    nextSmallerEle(arr, n);
+}
