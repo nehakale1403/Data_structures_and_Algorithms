285c58b4de87154571df981dc80a67c68d2f25da
Neha Kale <nkale8013@gmail.com>
2021-09-01 12:39:09 +0530

    rat in a maze problem

---
+++ Backtracking/254_Rat_in_a_Maze.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSafe(int** arr, int x, int y, int n){
+    if(x < n &&  y < n && arr[x][y] == 1){
+        return true;
+    }
+    return false;
+}
+
+bool ratInMaze(int** arr, int n, int x, int y, int** solArr){
+
+    cout<<"here"<<endl;
+
+    if(x == n-1 && y == n-1){
+        solArr[x][y] = 1;
+        return true;
+    }
+        
+    if(isSafe(arr, x, y, n)){
+        solArr[x][y] = 1;
+        if(ratInMaze(arr,n, x+1,y, solArr)){
+            return true;
+        }
+        if(ratInMaze(arr,n, x,y+1,solArr)){
+            return true;
+        }
+        solArr[x][y] = 0;   //backtracking
+        return false;
+    }
+    return false;
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+
+    int** arr = new int *[n];
+    for(int i=0;i<n; i++){
+        arr[i] = new int[n];
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int** solArr = new int *[n];
+    for(int i=0;i<n; i++){
+        solArr[i] = new int[n];
+        for(int j=0; j<n; j++){
+            solArr[i][j] = 0;
+        }
+    }
+
+
+    if(ratInMaze(arr,n, 0, 0, solArr)){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cout<<solArr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    } 
+}
