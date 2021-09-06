1bf9d9e367fa7e064309bd1385542d606eac8b16
Neha Kale <nkale8013@gmail.com>
2021-09-06 11:38:57 +0530

    graph coloring problem

---
+++ Backtracking/259_Graph_Colouring_Problem.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int v, e;
+    cin>>v>>e;
+    vector<vector<int>> g(v);
+    for(int i=0; i<e; i++){
+        int x, y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+
+    int res[v];
+    bool available[v];
+    res[0] = 0;
+    for(int i=1; i<v; i++) res[i] = -1;
+    for(int i=0; i<v; i++) available[i] = false;
+
+    int cn = 0;
+    for(int i=1; i<v; i++){
+        for(auto x:g[i]){
+            if(res[x] != -1){
+                available[res[x]] = true;
+            }
+        }
+
+        int cr;
+        for(cr=0; cr<v; cr++){
+            if(available[cr] == false){
+                break;
+            }
+        }
+        res[i] = cr;
+        cn = max(cn, cr+1);
+
+        //resetting the array
+        for(auto x: g[i]){
+            if(res[x]!= -1){
+                available[res[x]] = false;
+            }
+        }
+    }
+    cout<<cn<<endl;
+
+    for(int i=0; i<v; i++)
+        cout<<res[i]<<" ";
+}
