89293da429a6312af1ef8e1f5b83eea1976c7b06
Neha Kale <nkale8013@gmail.com>
2021-09-18 22:21:35 +0530

    Print all possible paths from top left to bottom right of a mXn matrix

--- Backtracking/270_All_Possible_Paths_from_topleft_bottomright.cpp
+++ Backtracking/270_All_Possible_Paths_from_topleft_bottomright.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+void printPaths(int sr,int sc, int dr,int dc, string path){
+
+    if(sr>dr || sc>dc){
+        return;
+    }
+    if(sr == dr && sc == dc){
+        cout<<path<<endl;
+        return;
+    }
+
+    printPaths(sr, sc+1, dr, dr, path + "h");
+    printPaths(sr+1, sc, dr, dc, path + "v");
     
+}
+
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    printPaths(1,1,n,m, "");
 }
