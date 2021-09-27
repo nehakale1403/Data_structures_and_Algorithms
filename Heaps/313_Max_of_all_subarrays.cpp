7ba29b27b8611b155ecd788d2dd2a3cb12c2f4d1
Neha Kale <nkale8013@gmail.com>
2021-09-27 22:31:45 +0530

    max of all subarrays

---
+++ Heaps/313_Max_of_all_subarrays.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i, j;
+
+    for(i=0, j=1; j<2, i<4; i++, j++){
+        cout<<i<<" "<<j<<endl;
+    }
+    cout<<i<<" "<<j;
+}
