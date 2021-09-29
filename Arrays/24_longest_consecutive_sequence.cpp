96894c195d603ebc8733fd1d068193db79afe922
Neha Kale <nkale8013@gmail.com>
2021-09-29 16:11:15 +0530

    longest consecutive sequence

--- Arrays/24_longest_consecutive_sequence.cpp
+++ Arrays/24_longest_consecutive_sequence.cpp
@@ -8,20 +8,39 @@ int main(){
     int arr[n];
     for(int i=0; i<n; i++) cin>>arr[i];
 
-    sort(arr, arr+n);
-    int max_count = 0;
-    int count=1;
+    set<int> s;
     for(int i=0; i<n; i++){
-        if(arr[i]+1 ==arr[i+1]){
-            count++;
-        }else if(arr[i] == arr[i+1]){
-            continue;
-        }else{
-            count=1;
-        }
-            
-        max_count = max(count, max_count);
+        s.insert(arr[i]);
     }
 
-    cout<<max_count;
+    int ans=0;
+    for(int i=0; i<n; i++){
+        if(s.find(arr[i]-1) == s.end()){
+        
+            int j = arr[i]+1;
+            while(s.find(j) != s.end()){
+                j++;
+            }
+        
+            ans = max(ans, j-arr[i]);
+        }
+    }
+
+    cout<<ans;
+    // sort(arr, arr+n);
+    // int max_count = 0;
+    // int count=1;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]+1 ==arr[i+1]){
+    //         count++;
+    //     }else if(arr[i] == arr[i+1]){
+    //         continue;
+    //     }else{
+    //         count=1;
+    //     }
+            
+    //     max_count = max(count, max_count);
+    // }
+
+    // cout<<max_count;
 }
--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
+ == s.end()
