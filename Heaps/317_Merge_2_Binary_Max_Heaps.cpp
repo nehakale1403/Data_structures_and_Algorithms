ada0dbefb6bdf8d7a8963099763ec950e476d7a4
Neha Kale <nkale8013@gmail.com>
2021-10-01 22:16:58 +0530

    merging 2 binary max heaps

--- Heaps/317_Merge_2_Binary_Max_Heaps.cpp
+++ Heaps/317_Merge_2_Binary_Max_Heaps.cpp
@@ -57,3 +57,30 @@ int main(){
   
     return 0;
 }
+
+//this is the efficient method
+
+// vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+//         // your code here
+//         vector<int> ans;
+//         int i=0,j=0;
+//         while(i<n and j<m){
+//             if(a[i]<b[j]){
+//                 ans.push_back(b[j]);
+//                 j++;
+//             }
+//             else{
+//                 ans.push_back(a[i]);
+//                 i++;
+//             }
+//         }
+//         while(i<n){
+//             ans.push_back(a[i]);
+//                 i++;
+//         }
+//         while(j<m){
+//             ans.push_back(b[j]);
+//                 j++;
+//         }
+//         return ans;
+// }
