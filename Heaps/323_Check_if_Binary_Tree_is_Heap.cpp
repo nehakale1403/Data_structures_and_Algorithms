3c1c894883580893720953f1f91249cca909fbe5
Neha Kale <nkale8013@gmail.com>
2021-10-08 14:26:01 +0530

    check if binary tree is a heap

---
+++ Heaps/323_Check_if_Binary_Tree_is_Heap.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+class Solution {
+  public:
+    bool isHeap(struct Node* root) {
+        // code here
+        if(root == NULL || (root->right == NULL && root->left== NULL)){
+            return true;
+        }
+        
+        if(root->left == NULL && root->right != NULL){
+            return false;
+        }
+        if(root->left != NULL && root->data < root->left->data){
+            return false;
+        }
+        if(root->right != NULL && root->data < root->right->data){
+            return false;
+        }
+    
+        return isHeap(root->left)&& isHeap(root->right);
+    }
+};
+
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        if (ob.isHeap(root))
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+} 
