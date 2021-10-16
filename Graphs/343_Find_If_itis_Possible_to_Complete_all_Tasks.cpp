cec2b547cc7b4b883d3370d165b43dbb96b00c3f
Neha Kale <nkale8013@gmail.com>
2021-10-16 14:04:33 +0530

    Find whether it is possible to finish all tasks or not from given dependencies

---
+++ Graphs/343_Find_If_itis_Possible_to_Complete_all_Tasks.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool dfs(vector<int> adj[], vector<int> &visited, int s, vector<int> &rest){
+
+    visited[s]=true;
+    rest[s]=true;
+       
+    for(int u:adj[s]){
+        if(visited[u]==false){
+           if(dfs(adj,visited,u,rest))
+                return true;
+        }else if(rest[u]==true)
+            return true;
+    }
+    rest[s]=false;
+    return false;
+
+}
+
+bool isCyclic(int v, vector<int> adj[]){
+
+    vector<int> visited(v+1, 0);
+    vector<int> rest(v, 0);
+    bool flag = false;
+    for(int i=1; i<=v; i++){
+        if(visited[i] == 0){
+            if(dfs(adj, visited, i, rest))
+                return true;
+        }
+    }
+    return false;
+
+}
+
+int main(){
+    int v, e;
+    cin>>v>>e;
+
+    //adjacency list
+    vector<int> g[v+1];
+    for(int i=0; i<e; i++){
+        int x, y;
+        cin>>x>>y;
+        g[x].push_back(y);
+    }
+
+    
+    if(isCyclic(v, g))
+        cout<<"All tasks cannot be completed"<<endl;
+    else
+        cout<<"All tasks can be completed"<<endl;
+
+}
