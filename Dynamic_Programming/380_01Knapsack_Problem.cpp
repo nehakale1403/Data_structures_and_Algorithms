c5b70967fe47e5b468ff8c6ebe672e73d4b55aa8
Neha Kale <nkale8013@gmail.com>
2021-12-02 22:06:49 +0530

    01 knapsack problem

---
+++ Dynamic_Programming/380_01Knapsack_Problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+knapsack01(n, w, arr)
+
+int main(){
+    int n, w;
+
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    knapsack01(n, w, arr);
+}
