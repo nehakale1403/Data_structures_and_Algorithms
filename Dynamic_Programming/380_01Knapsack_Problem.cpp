5b7d67e598d77a37361b4d09301364736794a99f
Neha Kale <nkale8013@gmail.com>
2021-12-03 21:41:59 +0530

    01 knapsack problem

--- Dynamic_Programming/380_01Knapsack_Problem.cpp
+++ Dynamic_Programming/380_01Knapsack_Problem.cpp
@@ -1,13 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-knapsack01(n, w, arr)
+const int N = 1e3+2;
+
+int dp[N][N];
+
+int knapsack01(int n, int w, int wt[], int profit[]){
+
+    if(w == 0 || n==0) return 0;
+
+    if(dp[n][w] != -1)
+        return dp[n][w];
+
+    int inc=0, exc=0, ans = 0;
+
+    if(wt[n-1] > w)
+        dp[n][w] = knapsack01(n-1, w, wt, profit);
+
+    dp[n][w] = max(knapsack01(n-1, w, wt, profit), knapsack01(n-1, w-wt[n-1], wt, profit) + profit[n-1]); 
+    return dp[n][w];
+}
 
 int main(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            dp[i][j] = -1;
+        }
+    }
     int n, w;
+    cin>>n>>w;
+    int weights[n];
+    int profit[n];
+    for(int i=0; i<n; i++) cin>>weights[i];
+    for(int i=0; i<n; i++) cin>>profit[i];
 
-    int arr[n];
-    for(int i=0; i<n; i++) cin>>arr[i];
-
-    knapsack01(n, w, arr);
+    cout<<knapsack01(n, w, weights, profit);
 }
