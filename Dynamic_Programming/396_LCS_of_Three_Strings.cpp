e355a752ebd70597c64f720c94c65bf92616cb97
Neha Kale <nkale8013@gmail.com>
2021-12-10 19:58:30 +0530

    lcs of 3 strings

--- Dynamic_Programming/396_LCS_of_Three_Strings.cpp
+++ Dynamic_Programming/396_LCS_of_Three_Strings.cpp
@@ -1,24 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int dp[205][205][205];
+
 int lcs(string &s1, string &s2, string &s3, int i, int j, int k){
 
     if(i == 0 || j == 0 || k == 0)
         return 0;
 
+    if(dp[i][j][k] != -1)
+        return dp[i][j][k];
+
     if(s1[i-1] == s2[j-1] && s2[j-1] == s3[k-1]){
 
-        return 1 + lcs(s1, s2, s3, i-1, j-1, k-1);
+        return dp[i][j][k] = 1 + lcs(s1, s2, s3, i-1, j-1, k-1);
     }
 
     int l = lcs(s1, s2, s3, i-1, j, k);
     int r = lcs(s1, s2, s3, i, j-1, k);
     int p = lcs(s1, s2, s3, i, j, k-1);
 
-    return max({l, r, p});
+    return dp[i][j][k] = max({l, r, p});
 }
 
 int main(){
+
+    memset(dp, -1, sizeof(dp));
     
     string s1, s2, s3;
     cin>>s1>>s2>>s3;
