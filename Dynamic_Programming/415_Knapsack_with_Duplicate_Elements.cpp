fdc7727078f1ee01f0e9d92fe80af30f7b883930
Neha Kale <nkale8013@gmail.com>
2021-12-29 11:14:56 +0530

    knapsack with duplicate elements

--- Dynamic_Programming/415_Knapsack_with_Duplicate_Elements.cpp
+++ Dynamic_Programming/415_Knapsack_with_Duplicate_Elements.cpp
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+map<pair<int, int>, int> memo;
+
 int res=0;
 
 int knapsack(int n, int profit[], int wt[], int capacity){
 
-    cout<<capacity<<endl;
+    // cout<<capacity<<endl;
 
     if(capacity == 0 || n == 0) return 0;
+    if(memo.find({n, capacity}) != memo.end()) return memo[{n, capacity}];
 
     if(capacity >= wt[n-1]){
-        return max(profit[n-1] + knapsack(n, profit, wt, capacity - wt[n-1]), knapsack(n-1,profit, wt, capacity));
+        return memo[{n, capacity}] = max(profit[n-1] + knapsack(n, profit, wt, capacity - wt[n-1]), knapsack(n-1,profit, wt, capacity));
     }else{
-        return knapsack(n-1, profit, wt, capacity);
+        return memo[{n, capacity}] = knapsack(n-1, profit, wt, capacity);
     }
 }
 
