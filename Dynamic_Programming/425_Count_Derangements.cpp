dfbcc9f45c00f63aaf7d6e42ecacf8b2c253e8f9
Neha Kale <nkale8013@gmail.com>
2021-12-31 15:07:24 +0530

    count derangements

---
+++ Dynamic_Programming/425_Count_Derangements.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countDerangements(int n){
+
+    if(n == 1) return 0;
+    if(n == 2) return 1;
+
+    return (n-1) * (countDerangements(n-1) + countDerangements(n-2));
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<<countDerangements(n);
+}
