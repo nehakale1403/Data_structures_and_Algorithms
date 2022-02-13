347e405a5cee839c9e8bb17b1921a21a68450152
Neha Kale <nkale8013@gmail.com>
2022-02-13 08:46:46 +0530

    creation of trie

--- Trie/373_Construct_Trie_from_scratch.cpp
+++ Trie/373_Construct_Trie_from_scratch.cpp
@@ -2,127 +2,80 @@
 using namespace std;
 
 
-
-struct trieNode{
+class trieNode{
     char data;
-    int wc;
-    trieNode *child[26];
-    
-    
+    int we;
+    trieNode *arr[26];
+
+    public:
+        trieNode* newNode(char c){
+            trieNode *t = new trieNode();
+            t->data = c;
+            t->we = 0;
+            for(int i=0; i<26; i++){
+                t->arr[i] = NULL;
+            }
+            return t;
+        }
+
+        void insertWord(string s, trieNode *root){
+            trieNode *curr = root;
+            for(int i=0; i<s.length(); i++){
+                cout<<"inserting "<<s[i]<<endl;
+                int index = s[i] - 'a';
+                if(curr->arr[index] == NULL)
+                    curr->arr[index] = newNode(s[i]);    
+                curr = curr->arr[index];
+            }
+            curr->we ++;
+        }
+
+        void deleteWord(string s, trieNode *root){
+            trieNode *curr = root;
+            for(int i=0; i<s.length(); i++){
+                int index = s[i] - 'a';
+                if(curr->arr[index] == NULL)
+                    return;
+                curr = curr->arr[index];
+            }
+            curr->we--;
+        }
+
+        bool search(string s, trieNode *root){
+            trieNode *curr = root;
+            for(int i=0; i<s.length(); i++){
+                cout<<"searching for "<<s[i]<<endl;
+                int index = s[i] - 'a';
+                if(curr->arr[index] == NULL)
+                    return false; 
+                curr = curr->arr[index];
+                if((i == s.length()-1) && curr->we == 0)
+                    return false;
+            }
+            return true;
+        }
+
+
 };
 
-    trieNode nodepool[100000];  //Pool of 100K trieNode
-    trieNode *root;    //Root of Trie
-    int poolcount;  //Always points to next free trieNode
 
-void init(){
-        poolcount = 0;
-        root = &nodepool[poolcount++];
-        root->data = NULL;
+int main(){
 
-        for(register int i=0; i<26; i++)
-            root->child[i] = NULL;
-    }
+    trieNode obj;
+    trieNode *root = obj.newNode('/');
+    obj.insertWord("hello", root);
+    obj.insertWord("hell", root);
+    obj.insertWord("neha", root);
+    obj.insertWord("avaneesh", root);
 
-    trieNode *getNode(char c){
-        trieNode *newnode = &nodepool[poolcount++];
-        newnode->data = c;
+    cout<<obj.search("hello", root)<<endl;
+    cout<<obj.search("neha", root)<<endl;
+    cout<<obj.search("avan", root)<<endl;
 
-        for(register int i=0; i<26; i++)
-            newnode->child[i] = NULL;
-        newnode->wc = 0;
-    }
+    obj.deleteWord("hello", root);
 
-    void insert(char *s){
-        trieNode *curr = root;
-        int index;
-        for(int i=0; s[i]!= '\0'; i++){
-            index = s[i] - 'a';
-            if(curr->child[index] == NULL){
-                curr->child[index] = getNode(s[i]);
-            }
-            curr->child[index]->wc +=1;
-            curr = curr->child[index];
-        }
-    }
-
-    bool search(char *s){
-        trieNode *curr = root;
-        int index;
-        for(int i=0; s[i]!='\0'; i++){
-            index = s[i] - 'a';
-            if(curr->child[index] == NULL || curr->child[index]->wc == 0)
-                return false;
-            curr = curr->child[index];
-        }
-        return true;
-    }
-
-    bool trieDelete(char *s){
-        if(search(s)){
-            trieNode *curr = root;
-            int index;
-            for(int i=0; s[i]!='\0'; i++){
-                index = s[i] - 'a';
-                curr->child[index]->wc -= 1;
-                curr = curr->child[index];
-            }
-        }
-    }
-
-    int countPrefix(char *s){
-        trieNode *curr = root;
-        int index;
-        for(int i=0; s[i]!='\0'; i++){
-            index = s[i] - 'a';
-            if(curr->child[index] == NULL || curr->child[index]->wc == 0)
-                return 0;
-            curr = curr->child[index];
-        }
-        return curr->wc;
-    }
+    cout<<obj.search("hell", root)<<endl;
+    cout<<obj.search("hello", root)<<endl;
 
 
-
-int main()
-{
-    init();
-    char a[5] = {'a','r','m','y'};
-    char b[5] = {'a','r','m'};
-    char c[5] = {'a','r','m','s'};
-    char d[5] = {'a','r','y'};
-    char e[5] = {'a','m','y'};
-    char f[5] = {'a','p','i'};
-
-
-    insert(a);
-    insert(b);
-    insert(c);
-    insert(d);
-    insert(e);
-    insert(f);
-
-    //cout<<search(b)<<"\n";
-
-    printf("No of strings with given prefix = %d\n",countPrefix("a"));
-    printf("No of strings with given prefix = %d\n",countPrefix("ar"));
-    printf("No of strings with given prefix = %d\n",countPrefix("arm"));
-    printf("No of strings with given prefix = %d\n",countPrefix("army"));
-    printf("No of strings with given prefix = %d\n",countPrefix("armi"));
-    printf("No of strings with given prefix = %d\n",countPrefix("ary"));
-
-    cout<<"Deletion...STARTED\n";
-    trieDelete(a);
-    trieDelete(d);
-    cout<<"DONE...\n\n";
-
-
-    printf("No of strings with given prefix = %d\n",countPrefix("a"));
-    printf("No of strings with given prefix = %d\n",countPrefix("ar"));
-    printf("No of strings with given prefix = %d\n",countPrefix("arm"));
-    printf("No of strings with given prefix = %d\n",countPrefix("army"));
-    printf("No of strings with given prefix = %d\n",countPrefix("armi"));
-    printf("No of strings with given prefix = %d\n",countPrefix("ary"));
-
-    return 0;
 }
